d1d907616f7ea3ec0b44faae847b571f8eefb5fe
Gabriel Oliveira <g.webdevelopr@gmail.com>
2020-06-28 11:21:12 -0300

    new data

--- src/data.ts
+++ src/data.ts
@@ -6,30 +6,81 @@ interface Card {
 
 const data: Card[] = [
     {
-        dirigente: 'Sadi Frandoloso',
+        dirigente: 'Josemberg',
         auxiliar: 'Vitor Malagoli',
-        data: '6 de Junho'
+        data: '28 de Junho'
     },
     {
-        dirigente: 'Sadi Frandoloso',
-        auxiliar: 'Vitor Malagoli',
-        data: '6 de Junho'
+        dirigente: 'Júlio',
+        auxiliar: 'Richard',
+        data: '4 de Julho'
     },
     {
-        dirigente: 'Sadi Frandoloso',
-        auxiliar: 'Vitor Malagoli',
-        data: '6 de Junho'
+        dirigente: 'Israel Cerqueira',
+        auxiliar: 'Israel Cerqueira',
+        data: '5 de Julho'
     },
     {
-        dirigente: 'Sadi Frandoloso',
-        auxiliar: 'Vitor Malagoli',
-        data: '6 de Junho'
+        dirigente: 'Richard',
+        auxiliar: 'Richard',
+        data: '11 de Julho'
     },
     {
-        dirigente: 'Sadi Frandoloso',
+        dirigente: 'Sadi',
+        auxiliar: 'Sadi',
+        data: '12 de Julho'
+    },
+    {
+        dirigente: 'Diorge',
+        auxiliar: 'Gabriel',
+        data: '18 de Julho'
+    },
+    {
+        dirigente: 'Malagoli',
+        auxiliar: 'Malagoli',
+        data: '19 de Julho'
+    },
+    {
+        dirigente: 'Israel',
+        auxiliar: 'Patrik',
+        data: '25 de Julho'
+    },
+    {
+        dirigente: 'Aloísio',
+        auxiliar: 'Vitor',
+        data: '26 de Julho'
+    },
+    {
+        dirigente: 'Vitor Malagoli',
         auxiliar: 'Vitor Malagoli',
-        data: '6 de Junho'
-    }
+        data: '1 de Agosto'
+    },
+    {
+        dirigente: 'Josemberg',
+        auxiliar: 'Gabriel Oliveira',
+        data: '2 de Agosto'
+    },
+    {
+        dirigente: 'Paulo',
+        auxiliar: 'Richard',
+        data: '8 de Agosto'
+    },
+    {
+        dirigente: 'Israel Cerqueira',
+        auxiliar: 'Israel Cerqueira',
+        data: '9 de Agosto'
+    },
+    {
+        dirigente: 'Patrik França',
+        auxiliar: 'Patrik França',
+        data: '15 de Agosto'
+    },
+    {
+        dirigente: 'Milton Melo',
+        auxiliar: 'Gabriel Oliveira',
+        data: '15 de Agosto'
+    },
+
 ]
 
 export default data
