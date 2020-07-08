64da0355fb6a0059223105fe12817b42fd990781
Gabriel Oliveira <g.webdevelopr@gmail.com>
2020-07-07 21:18:02 -0300

    moving server folder

--- server/src/controllers/peopleController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import Users from '../db/schemas/users'
-
-export default class PeopleController {
-    async index(req: Request, res: Response) {
-        return await Users.find()
-    }
-}
